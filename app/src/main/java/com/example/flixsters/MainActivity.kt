d98b1f6de4ba8aabc1817b28a723fb49553b718e
Jaylyn Lowes <jlow7077@students.vsu.edu>
2025-09-18 15:04:09 -0400

    Finish main activity, connectiong everything and making test in case internet did not work

--- app/src/main/java/com/example/flixsters/MainActivity.kt
+++ app/src/main/java/com/example/flixsters/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.flixsters
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rv: RecyclerView
+    private lateinit var progress: ProgressBar
+    private val adapter = MoviesAdapter()
+
+    private val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +29,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        rv = findViewById(R.id.rvMovies)
+        progress = ProgressBar(this).apply { visibility = View.GONE }
+
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
+
+        fetchNowPlaying()
+    }
+
+    private fun fetchNowPlaying() {
+        progress.visibility = View.VISIBLE
+        lifecycleScope.launch {
+            try {
+                val resp = RetrofitInstance.api.getNowPlaying(API_KEY)
+                if (resp.isSuccessful) {
+                    val movies = resp.body()?.results.orEmpty()
+                    adapter.submit(movies)
+                } else {
+                    Toast.makeText(this@MainActivity,
+                        "Error: ${resp.code()}",
+                        Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this@MainActivity,
+                    "Network error: ${e.localizedMessage}",
+                    Toast.LENGTH_SHORT).show()
+            } finally {
+                progress.visibility = View.GONE
+            }
         }
     }
 }
